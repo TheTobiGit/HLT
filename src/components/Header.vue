ec0e412cd2992425ea7dc3a0d4926c160ff25a3f
Gideon Sarfo <aemonsarfo@outlook.com>
2023-08-18 01:00:20 -0400

    header done

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,13 +4,13 @@
       class="h-[10vh] flex items-center w-screen overflow-hidden text-brandtext px-5 md:px-10"
     >
       <div
-        class="md:w-1/4 lg:w-1/3 h-full uppercase flex items-center font-bold text-3xl border border-green-600"
+        class="md:w-1/4 lg:w-1/3 h-full uppercase flex items-center font-bold text-3xl border"
       >
         <p class="cursor-pointer">{{ logo }}</p>
       </div>
       <div
         v-show="navShow"
-        class="absolute top-0 left-0 w-screen z-20 md:z-0 bg-white md:bg-transparent flex-col md:flex-row justify-center gap-6 md:static md:w-2/4 lg:w-1/3 h-screen md:h-full flex capitalize items-center md:justify-evenly md:gap-0 font-normal border border-blue-600"
+        class="absolute top-0 left-0 w-screen z-20 md:z-0 bg-white md:bg-transparent flex-col md:flex-row justify-center gap-6 md:static md:w-2/4 lg:w-1/3 h-screen md:h-full flex capitalize items-center md:justify-evenly md:gap-0 font-normal"
       >
         <ul v-for="items in navItems" :key="items" class="z-30">
           <li class="cursor-pointer">{{ items }}</li>
@@ -20,7 +20,7 @@
         </ul>
       </div>
       <div
-        class="md:w-1/4 lg:w-1/3 h-full hidden md:flex items-center justify-end border border-pink-600"
+        class="md:w-1/4 lg:w-1/3 h-full hidden md:flex items-center justify-end border"
       >
         <button
           class="uppercase border p-2 lg:w-[8vw] border-brand text-brand font-semibold"
@@ -40,7 +40,7 @@
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, onMounted, onUnmounted } from "vue";
 
 export default {
   setup() {
@@ -51,9 +51,36 @@ export default {
 
     const toggleNav = () => {
       navShow.value = !navShow.value;
-
       console.log("clicked");
     };
+
+    // Function to handle initial visibility of navShow
+    const setInitialNavVisibility = () => {
+      if (window.innerWidth >= 768) {
+        navShow.value = true;
+      }
+      if (window.innerWidth < 768) {
+        navShow.value = false;
+      }
+    };
+
+    // Call the function when the component is mounted
+    onMounted(() => {
+      setInitialNavVisibility();
+    });
+
+    // Update navShow on window resize
+    const handleResize = () => {
+      setInitialNavVisibility();
+    };
+
+    // Listen for window resize events
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup the event listener when the component is unmounted
+    onUnmounted(() => {
+      window.removeEventListener("resize", handleResize);
+    });
 
     return {
       navItems,
